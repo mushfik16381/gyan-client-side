2d0cd9b5122969ee644a6ddbf708698f56c3ee69
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-27 05:12:19 +0600

    faq part

--- src/Pages/Faq/Faq.js
+++ src/Pages/Faq/Faq.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Faq = () => {
     return (
-        <div>
-            <h4>Question</h4>
+        <div >
+            <Container>
+                <h4 className='text-center mt-5'>This Page Is Cokking</h4>
+            </Container>
         </div>
     );
 };
