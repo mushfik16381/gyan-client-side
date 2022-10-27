81f6892bec1419f20a360b64f59ec623580d7321
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-27 10:26:35 +0600

    register added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Login.css'
 
 const Login = () => {
-    
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password)
+    }
     return (
         <div className='w-25 mx-auto login-container'>
             <h3 className='text'>Please Login Now!</h3>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="email" placeholder="Enter email" required/>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" required/>
                 </Form.Group>
                 <button className='login'>Login</button>
-                {/* <Button variant="primary" type="submit">
-                    Login
-                </Button> */}
             </Form>
         </div>
     );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password, name)
+    }
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='w-25 mx-auto login-container'>
+            <h3 className='text'>Please Register Now!</h3>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name="name" placeholder="Enter name" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" required/>
+                </Form.Group>
+                <Link to="/login"><span style={{ color:'#fff'}}>Already, have an account?</span></Link>
+                <button className='login'>Register</button>
+            </Form>
         </div>
     );
 };
