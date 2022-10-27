ab2d83190d85c95277b9d7edb880ddd3042cf016
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-27 06:07:29 +0600

    error page

---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Error.css'
+
+const Error = () => {
+    return (
+        <div className='error-container'>
+            <Link to={'/'}><button className='btn-error'>Back to Home</button></Link>
+        </div>
+    );
+};
+
+export default Error;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Blog from "../../Pages/Blog/Blog";
 import Courses from "../../Pages/Courses/Courses";
+import Error from "../../Pages/Error/Error";
 import Faq from "../../Pages/Faq/Faq";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -40,5 +41,6 @@ export const routes = createBrowserRouter([
     {
         path: '/register',
         element: <Register></Register>
-    }
+    },
+    {path: '*', element: <Error></Error>}
 ])
